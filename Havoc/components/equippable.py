f9428374e5c889eff059bef877737738573b0e8e
FatBunny13 <40076909+FatBunny13@users.noreply.github.com>
2018-08-18 15:21:24 -0400

    Add files via upload

---
+++ Havoc/components/equippable.py
@@ -0,0 +1,7 @@
+class Equippable:
+    def __init__(self, slot, power_bonus=0, defense_bonus=0, max_hp_bonus=0, agility_bonus=0):
+        self.slot = slot
+        self.power_bonus = power_bonus
+        self.defense_bonus = defense_bonus
+        self.max_hp_bonus = max_hp_bonus
+        self.agility_bonus = agility_bonus
